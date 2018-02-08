c3589f5743f3f39cdd3c884e08db72a844a95b4d
cbartholomew69 <bartholomew_christy@yahoo.com>
2018-02-08 15:12:16 -0500

    set up router

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'about',
-  templateUrl: 'about.component.html',
-  styleUrls: ['about.component.css']
+  templateUrl: 'about.component.html'
 })
 export class AboutComponent { }
--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'search',
-  templateUrl: 'search.component.html',
-  styleUrls: ['search.component.css']
+  templateUrl: 'search.component.html' 
 })
 export class SearchComponent { }
