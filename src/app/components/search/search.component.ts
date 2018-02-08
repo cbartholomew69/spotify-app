f1aaf6eaf21df851dd660c398d98d10696b60978
cbartholomew69 <bartholomew_christy@yahoo.com>
2018-02-08 16:25:33 -0500

    modify css files

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'search',
-  templateUrl: 'search.component.html' 
+  templateUrl: '/search.component.html'
 })
 export class SearchComponent { }
